b9b09737bb6f8a65caf276e14ed21c71a1d8ecc8
Aaron Meaney <aaronmeaney@hotmail.com>
2021-03-30 18:02:38 +0100

    Clean up tests to make them more consistent.

--- src/test/core/math/tuple_test.cpp
+++ src/test/core/math/tuple_test.cpp
@@ -28,50 +28,50 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, tuple_with_w0_is_a_vector)
 TEST(CppRayTracerChallenge_Core_Math_Tuple, buildPoint_will_build_tuple_with_w1)
 {
 	Tuple point = Tuple::buildPoint(4.3f, -4.2f, 3.1f);
-	Tuple tuple(4.3f, -4.2f, 3.1f, 1.0f);
-	EXPECT_EQ(point, tuple);
+	Tuple expectedResult(4.3f, -4.2f, 3.1f, 1.0f);
+	EXPECT_EQ(point, expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, buildVector_will_build_tuple_with_w0)
 {
 	Tuple vector = Tuple::buildVector(4.3f, -4.2f, 3.1f);
-	Tuple tuple(4.3f, -4.2f, 3.1f, 0.0f);
-	EXPECT_EQ(vector, tuple);
+	Tuple expectedResult(4.3f, -4.2f, 3.1f, 0.0f);
+	EXPECT_EQ(vector, expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, zeroVector_will_build_zero_vector)
 {
 	Tuple vector = Tuple::zeroVector();
-	Tuple tuple(0, 0, 0, 0);
-	EXPECT_EQ(vector, tuple);
+	Tuple expectedResult(0, 0, 0, 0);
+	EXPECT_EQ(vector, expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, equality_true)
 {
-	Tuple tupleA(4.3f, -4.2f, 3.1f, 1.0f);
-	Tuple tupleB(4.3f, -4.2f, 3.1f, 1.0f);
-	EXPECT_TRUE(tupleA == tupleB);
+	Tuple tuple(4.3f, -4.2f, 3.1f, 1.0f);
+	Tuple expectedResult(4.3f, -4.2f, 3.1f, 1.0f);
+	EXPECT_TRUE(tuple == expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, equality_false)
 {
-	Tuple tupleA(4.3f, -4.2f, 3.1f, 1.0f);
-	Tuple tupleB(1.0f, 2.0f, -9.3f, 0.0f);
-	EXPECT_FALSE(tupleA == tupleB);
+	Tuple tuple(4.3f, -4.2f, 3.1f, 1.0f);
+	Tuple expectedResult(1.0f, 2.0f, -9.3f, 0.0f);
+	EXPECT_FALSE(tuple == expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, inequality_true)
 {
-	Tuple tupleA(4.3f, -4.2f, 3.1f, 1.0f);
-	Tuple tupleB(1.0f, 2.0f, -9.3f, 0.0f);
-	EXPECT_TRUE(tupleA != tupleB);
+	Tuple tuple(4.3f, -4.2f, 3.1f, 1.0f);
+	Tuple expectedResult(1.0f, 2.0f, -9.3f, 0.0f);
+	EXPECT_TRUE(tuple != expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, inequality_false)
 {
-	Tuple tupleA(4.3f, -4.2f, 3.1f, 1.0f);
-	Tuple tupleB(4.3f, -4.2f, 3.1f, 1.0f);
-	EXPECT_FALSE(tupleA != tupleB);
+	Tuple tuple(4.3f, -4.2f, 3.1f, 1.0f);
+	Tuple expectedResult(4.3f, -4.2f, 3.1f, 1.0f);
+	EXPECT_FALSE(tuple != expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, addition)
@@ -80,7 +80,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, addition)
 	Tuple tupleB(-2, 3, 1, 0);
 	Tuple expectedResult(1, 1, 6, 1);
 
-	EXPECT_TRUE((tupleA + tupleB) == expectedResult);
+	EXPECT_EQ((tupleA + tupleB), expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_two_points_is_a_vector)
@@ -89,7 +89,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_two_points_is_a_vector)
 	Tuple pointB = Tuple::buildPoint(5, 6, 7);
 	Tuple expectedResult = Tuple::buildVector(-2, -4, -6);
 
-	EXPECT_TRUE((pointA - pointB) == expectedResult);
+	EXPECT_EQ((pointA - pointB), expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_a_vector_from_a_point_is_a_point)
@@ -98,7 +98,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_a_vector_from_a_point_is
 	Tuple vector = Tuple::buildVector(5, 6, 7);
 	Tuple expectedResult = Tuple::buildPoint(-2, -4, -6);
 
-	EXPECT_TRUE((point - vector) == expectedResult);
+	EXPECT_EQ((point - vector), expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_two_vectors_is_a_vector)
@@ -107,7 +107,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_two_vectors_is_a_vector)
 	Tuple vectorB = Tuple::buildVector(5, 6, 7);
 	Tuple expectedResult = Tuple::buildVector(-2, -4, -6);
 
-	EXPECT_TRUE((vectorA - vectorB) == expectedResult);
+	EXPECT_EQ((vectorA - vectorB), expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_a_vector_from_zero_vector_results_in_negation)
@@ -116,7 +116,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, subtracting_a_vector_from_zero_vecto
 	Tuple vector = Tuple::buildVector(1, -2, 3);
 	Tuple expectedResult = Tuple::buildVector(-1, 2, -3);
 
-	EXPECT_TRUE((zero - vector) == expectedResult);
+	EXPECT_EQ((zero - vector), expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, unary_negation_will_negate_tuple)
@@ -124,7 +124,7 @@ TEST(CppRayTracerChallenge_Core_Math_Tuple, unary_negation_will_negate_tuple)
 	Tuple vector = Tuple(1, -2, 3, -4);
 	Tuple expectedResult = Tuple(-1, 2, -3, 4);
 
-	EXPECT_TRUE(-vector == expectedResult);
+	EXPECT_EQ(-vector, expectedResult);
 }
 
 TEST(CppRayTracerChallenge_Core_Math_Tuple, multiplication_by_scalar)
