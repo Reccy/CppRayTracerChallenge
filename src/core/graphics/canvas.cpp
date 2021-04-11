a81c88d1530276124622fcd616cb0f16ceabfb81
Aaron Meaney <aaronmeaney@hotmail.com>
2021-04-11 17:08:43 +0100

    Add ballistic render to main program.

--- src/core/graphics/canvas.cpp
+++ src/core/graphics/canvas.cpp
@@ -4,5 +4,13 @@ using namespace CppRayTracerChallenge::Core::Graphics;
 
 void Canvas::writePixel(int x, int y, const Color color)
 {
+	if (x < 0 || x >= this->m_width) {
+		return;
+	}
+
+	if (y < 0 || y >= this->m_height) {
+		return;
+	}
+
 	m_colors[this->pixelIndexAt(x,y)] = color;
 }
--- src/main/win32_main.cpp
+++ src/main/win32_main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <fstream>
+#include <Windows.h>
+#include <ShlObj.h>
+#include <tchar.h>
+
 #include "math/vector.h"
 #include "math/point.h"
+#include "graphics/canvas.h"
+#include "graphics/color.h"
+#include "serializer/portable_pixmap_image_serializer.h"
 
 using namespace CppRayTracerChallenge::Core;
 
 struct Projectile {
 	Math::Point position = Math::Point(0, 1, 0);
-	Math::Vector velocity = Math::Vector(1, 1, 0).normalize();
+	Math::Vector velocity = Math::Vector(1, 1.8, 0).normalize() * 11.25;
 };
 
 struct Environment {
@@ -71,6 +79,11 @@ std::string log(const int currentTicks, const int maxTicks, const Projectile pro
 	return ss.str();
 }
 
+float lerp(float a, float b, float f)
+{
+	return a + f * (b - a);
+}
+
 int main()
 {
 	Projectile projectile;
@@ -79,12 +92,60 @@ int main()
 	int currentTicks = 0;
 	int maxTicks = 1000;
 
+	Graphics::Color c(0.8f, 0.2f, 0.2f);
+	Graphics::Canvas canvas(900, 550, Graphics::Color(0.5f, 0.1f, 0.6f));
+
+	Math::Point oldPos(0, 0, 0);
+	Math::Point newPos(0, 0, 0);
+
 	while ((projectile.position.y() > 0) && currentTicks < maxTicks)
 	{
+		oldPos = newPos;
+
 		projectile = tick(projectile, environment);
+
+		newPos = projectile.position;
+
+		for (int i = 0; i < 100; i++)
+		{
+			int xPos = (int)lerp((float)oldPos.x(), (float)newPos.x(), i / 100.0f);
+			int yPos = (int)lerp((float)oldPos.y(), (float)newPos.y(), i / 100.0f);
+
+			canvas.writePixel(xPos, canvas.height() - yPos, c);
+		}
+
 		currentTicks++;
 
 		std::cout << log(currentTicks, maxTicks, projectile, environment) << std::endl;
+	}
+
+	Serializer::PortablePixmapImageSerializer ppm;
+	ppm.serialize(canvas);
+
+	std::vector<char> ppmBuffer = ppm.buffer();
+
+	std::string bufferData(ppmBuffer.begin(), ppmBuffer.end());
+	
+	TCHAR appData[MAX_PATH];
+	if (SUCCEEDED(SHGetFolderPath(NULL,
+		CSIDL_DESKTOPDIRECTORY | CSIDL_FLAG_CREATE,
+		NULL,
+		SHGFP_TYPE_CURRENT,
+		appData))) {
+		std::basic_ostringstream<TCHAR> filePath;
+		filePath << appData << _TEXT("\\generated_image.ppm");
+
+		std::ofstream file;
+		file.open(filePath.str().c_str());
+
+		file << bufferData << std::endl;
+		file.close();
+
+		std::cout << "File written" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to write file" << std::endl;
 	}
 
 	return 0;
