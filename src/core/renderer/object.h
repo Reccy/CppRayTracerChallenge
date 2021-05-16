580574a5c2ad9b032cdbfb3cc25c1c5eefd15cb5
Aaron Meaney <aaronmeaney@hotmail.com>
2021-05-16 12:51:08 +0100

    Add comments to Object

--- src/core/renderer/object.h
+++ src/core/renderer/object.h
@@ -7,14 +7,33 @@
 
 namespace CppRayTracerChallenge::Core::Renderer
 {
+	/// <summary>
+	/// Represents an object within the rendered world.
+	/// The Object is composed of a shape and a material
+	/// </summary>
 	class Object
 	{
 	public:
 		Object(Math::Sphere shape);
 		Object(Math::Sphere shape, Material material);
 
+		/// <summary>
+		/// Returns the material assigned to the object
+		/// </summary>
+		/// <returns>Material assigned to the object</returns>
 		const Material material() const;
+
+		/// <summary>
+		/// Assigns a material to the object
+		/// </summary>
+		/// <param name="material">Material to assign</param>
 		void material(const Material material);
+
+		/// <summary>
+		/// Intersects a ray on the shape
+		/// </summary>
+		/// <param name="ray">The ray to intersect through the shape</param>
+		/// <returns>The Intersections between the ray and the shape</returns>
 		Math::Intersections intersect(const Math::Ray& ray) const;
 	private:
 		Math::Sphere m_shape;
