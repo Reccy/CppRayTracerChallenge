f3ba8c69114cd470bfd53dcd7032662669b872bc
Aaron Meaney <aaronmeaney@hotmail.com>
2021-07-26 12:38:07 +0100

    Add normal and normalLocal to SmoothTriangle that accepts uv instead of Point

--- src/core/math/smooth_triangle.cpp
+++ src/core/math/smooth_triangle.cpp
@@ -5,6 +5,18 @@ using namespace CppRayTracerChallenge::Core::Math;
 SmoothTriangle::SmoothTriangle(Point p1, Point p2, Point p3, Vector n1, Vector n2, Vector n3)
 	: Triangle(p1, p2, p3), m_n1(n1), m_n2(n2), m_n3(n3) {};
 
+const Vector SmoothTriangle::normal(const double u, const double v) const
+{
+	return normalLocal(u, v);
+}
+
+const Vector SmoothTriangle::normalLocal(const double u, const double v) const
+{
+	return Vector(m_n2 * u +
+		m_n3 * v +
+		m_n1 * (1.0 - u - v)).normalize();
+}
+
 Vector SmoothTriangle::n1() const
 {
 	return m_n1;
