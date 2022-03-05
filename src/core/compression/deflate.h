4b47430341e15c2ebe3868dfafb694ec7612dee9
Aaron Meaney <aaronmeaney@hotmail.com>
2022-03-05 20:34:06 +0000

    Setup Deflate class scaffold

---
+++ src/core/compression/deflate.h
@@ -0,0 +1,14 @@
+#ifndef _CPPRAYTRACERCHALLENGE_CORE_COMPRESSION_DEFLATE
+#define _CPPRAYTRACERCHALLENGE_CORE_COMPRESSION_DEFLATE
+
+#include "huffman_coding.h"
+
+namespace CppRayTracerChallenge::Core::Compression
+{
+	class Deflate
+	{
+
+	};
+}
+
+#endif _CPPRAYTRACERCHALLENGE_CORE_COMPRESSION_DEFLATE
---
+++ src/test/core/compression/deflate_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "compression/deflate.h"
+
+TEST(CppRayTracerChallange_Core_Compression_Deflate, ditto)
+{
+
+}
