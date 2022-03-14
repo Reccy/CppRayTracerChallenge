fd8a2caf547432dd2b958f932d1a8870eb114702
Aaron Meaney <aaronmeaney@hotmail.com>
2022-03-14 14:07:56 +0000

    Add another Adler32 test

--- src/test/core/encryption/adler32_test.cpp
+++ src/test/core/encryption/adler32_test.cpp
@@ -3,11 +3,22 @@
 
 using namespace CppRayTracerChallenge::Core::Encryption;
 
-TEST(CppRayTracerChallenge_Core_Encryption_Adler32, returns_expected_result)
+TEST(CppRayTracerChallenge_Core_Encryption_Adler32, returns_expected_result_0)
 {
 	std::vector<unsigned char> bytes = { 'h', 'e', 'l', 'l', 'o' };
 
 	unsigned int expectedResult = 0x062C0215;
+
+	unsigned int result = Adler32::run(bytes);
+
+	EXPECT_EQ(expectedResult, result);
+}
+
+TEST(CppRayTracerChallenge_Core_Encryption_Adler32, returns_expected_result_1)
+{
+	std::vector<unsigned char> bytes = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	unsigned int expectedResult = 0x091E01DE;
 
 	unsigned int result = Adler32::run(bytes);
 
