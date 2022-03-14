8317b35c93629a680758ed190166c368bdd68448
Aaron Meaney <aaronmeaney@hotmail.com>
2022-03-14 13:59:21 +0000

    Remove CRC private methods from header

--- src/core/encryption/crc.h
+++ src/core/encryption/crc.h
@@ -10,15 +10,6 @@
 
 namespace CppRayTracerChallenge::Core::Encryption::CRC
 {
-	// PRIVATE
-	void m_makeCrcTable(void);
-	
-	// PRIVATE
-	unsigned long m_updateCrc(unsigned long crc, unsigned char* buf, int len);
-
-	// PRIVATE
-	std::vector<unsigned char> m_longToBytes(unsigned long value);
-
 	// Return the CRC of the passed in bytes
 	std::vector<unsigned char> run(std::vector<unsigned char> vec);
 }
