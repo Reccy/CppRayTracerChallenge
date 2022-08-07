8ed595fc6c6e69d77d16e6ed6fe67ce4952aaf75
Aaron Meaney <aaronmeaney@hotmail.com>
2022-08-07 18:23:49 +0100

    Add any key prompt at end of test

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,5 +1,13 @@
 #include "gtest/gtest.h"
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+
+	auto result = RUN_ALL_TESTS();
+
+	std::cout << "\nTest Suite Finished\n"
+		<< "Press any key to continue" << std::endl;
+
+	std::cin.get();
+
+	return result;
 }
